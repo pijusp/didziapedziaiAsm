4abd2f2cb572d44b88b6749ebc8f9a186dfaf21d
Pijus <p.pozera@gmail.com>
2023-01-31 21:02:59 +0200

    Initial commit

---
+++ 001/app.js
@@ -0,0 +1,8 @@
+console.log("Hello!!");
+
+const array = Array(10).fill().map(() => Math.ceil(10 * Math.random()));
+
+// for (var i = 0; i < array.length; i++) {
+//     array[i] = array[i].toFixed(0);
+// }
+console.log(array);
